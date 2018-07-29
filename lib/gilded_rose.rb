5be499b9c43427cc8c43670176dd55bf027f4f9f
bhagath <bhagath@getcustomexposure.com>
2018-07-29 15:53:10 +0530

    tried when case for based on item name

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -4,42 +4,47 @@ SULFURAS = 'Sulfuras, Hand of Ragnaros'
 
 def update_quality(items)
   items.each do |item|
-    if item.name != AGED_BRIBE && item.name != BACKSTAGE_PASS
-      if item.quality > 0
-        if item.name != SULFURAS
-          item.quality -= 1
-        end
-      end
-    else
-      if item.quality < 50
-        item.quality += 1
-        if item.name == BACKSTAGE_PASS
-          if item.sell_in < 11
-            item_update_quantity(item, 1)
-          end
-          if item.sell_in < 6
-           item_update_quantity(item, 1)
-          end
-        end
-      end
-    end
 
     if item.name != SULFURAS
       item.sell_in -= 1
     end
 
-    if item.sell_in < 0
+    case item.name
+    when BACKSTAGE_PASS
+      item_update_quantity(item, 1)
+        if item.sell_in < 10
+          item_update_quantity(item, 1)
+        end
+        if item.sell_in < 5
+          item_update_quantity(item, 1)
+        end
+        item_update_quantity(item, -item.quality) if expired?(item)
+    when AGED_BRIBE
+      item_update_quantity(item, 1)
+       item_update_quantity(item, 1) if expired?(item)
+    when SULFURAS
+
+    else
+      item_update_quantity(item, -1)
+      item_update_quantity(item, -1) if expired?(item)
+    end
+
+    
+    if expired?(item)
       if item.name == AGED_BRIBE
-        item_update_quantity(item, 1)
+      
       elsif item.name == BACKSTAGE_PASS
-        item_update_quantity(item, -item.quality)
       elsif item.name = SULFURAS
 
       else
-        item_update_quantity(item, -1)        
+           
       end
     end
   end
+end
+
+def expired?(item)
+  item.sell_in < 0
 end
 
 def item_update_quantity(item,  quality_data)
